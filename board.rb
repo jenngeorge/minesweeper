6fc88cd3ffb55adfc011d792803d3140bbcb2a8f
Your Name <you@example.com>
2016-07-14 14:37:01 -0700

    placed bombs, began fringes

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 require_relative "tile"
+require 'byebug'
 require "colorize"
 
 class Board
@@ -8,14 +9,16 @@ class Board
   def initialize(size=8, bomb_count=12)
     @size = size
     @bomb_count = bomb_count
-    @grid = Array.new(size) {Array.new(size, Tile.new)}
+    @grid = Array.new(size) {Array.new(size) {Tile.new}}
+    set_bombs
+    set_fringes
   end
 
   def set_bombs
     flat_grid = @grid.flatten
     until @bomb_count == 0
       random_tile = flat_grid.sample
-      unless random_tile.bomb?
+      unless random_tile.bomb
         random_tile.bomb = true
         @bomb_count -= 1
       end
@@ -25,7 +28,7 @@ class Board
   def render_board
     grid.each do | row |
       row.each do | tile |
-        if tile.hidden?
+        if tile.hidden
           print "   ".colorize(:background => :light_blue)
         elsif tile.fringe_value == 0
           print "   ".colorize(:background => :white)
@@ -36,6 +39,40 @@ class Board
       print "\n"
     end
   end
+
+  def set_fringes
+    @grid.each_with_index do | row, row_index |
+      row.each_with_index do | tile, tile_index |
+        tile.fringe_value = find_near_bombs(row_index, tile_index)
+      end
+    end
+  end
+
+  def find_near_bombs(*pos)
+    x, y = pos
+    current_tile = self[x, y]
+    bombs = 0
+    (-1..1).each do |add_x|
+      (-1..1).each do |add_y|
+        x, y = x + add_x, y+add_y
+        next if add_x == 0 && add_y == 0
+        next if !valid_pos?(x, y)
+        bombs +=1 if self[x, y].bomb
+      end
+    end
+    bombs
+  end
+
+  def valid_pos?(*pos)
+    x, y = pos
+    x.between?(0, @size-1) && y.between?(0, @size-1)
+  end
+
+  def [](*pos)
+    x, y = pos
+    @grid[x][y]
+  end
+
 end
 
 
--- tile.rb
+++ tile.rb
@@ -1,16 +1,11 @@
 class Tile
+
+  attr_accessor :fringe_value, :hidden, :bomb
+
   def initialize
     @hidden = true
     @bomb = false
     @fringe_value = 0
-  end
-
-  def bomb?
-    @bomb
-  end
-
-  def hidden?
-    @hidden
   end
 
 end
